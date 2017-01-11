77f6e19e79a7bca61a9999d2122e93138f0a3a10
netoudi <netoudi@rocketmail.com>
2017-01-11 15:36:56 -0200

    Adicionando Laravel-Modules

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Bootstrapper\BootstrapperL5ServiceProvider::class,
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -252,6 +253,7 @@ return [
         'Tabbable' => Bootstrapper\Facades\Tabbable::class,
         'Table' => Bootstrapper\Facades\Table::class,
         'Thumbnail' => Bootstrapper\Facades\Thumbnail::class,
+        'Module' => Nwidart\Modules\Facades\Module::class,
 
     ],
 
